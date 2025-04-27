f9eb9c838074e1f29d30a397c7967db2c6492247
sunnyrainsky <115min@sina.com>
2025-04-27 10:14:22 +0800

    udpate setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my_project",                     # 项目名称
+    name="todo_manager",                     # 项目名称
     version="0.1.0",                       # 版本号
-    author="Your Name",                    # 作者
-    author_email="your.email@example.com", # 作者邮箱
-    description="A short description",     # 项目简介
+    author="jackmin-sw",                    # 作者
+    author_email="jackmin.sw@gmail.com", # 作者邮箱
+    description="Python todo list",     # 项目简介
     long_description=open("README.md").read(),  # 详细描述，通常从 README 读取
     long_description_content_type="text/markdown",  # 描述格式
-    url="https://github.com/yourname/my_project",  # 项目主页
+    url="https://github.com/jackmin-sw/todo_manager",  # 项目主页
     packages=find_packages(where="src"),
     package_dir={"": "src"},              # 自动查找所有 Python 包
     install_requires=[                     # 依赖列表
